89be7a581ed02df13e3d3472eaf8afbe09b1dbf2
SUK HWA HAM <hamsukhwa@gmail.com>
2018-06-07 21:24:15 -0400

    Blockchain API

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,16 @@ const PORT = 3000;
 
 const app = express();
 app.use(bodyParser.json());
-app.use(morgan("combine"));
+app.use(morgan("combined"));
+
+app.get("/blocks", (req, res) => {
+    res.send(getBlockchain());
+});
+
+app.post("/blocks", (req, res) => {
+    const { body: { data } } = req;
+    const newBlock = createNewBlock(data);
+    res.send(newBlock);
+});
+
 app.listen(PORT, () => console.log(`SH-coin Server is running on ${PORT}`));
