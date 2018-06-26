7b0a1ed8dbaa1fa21f2a201cf0052c9c6dd59b69
SUK HWA HAM <hamsukhwa@gmail.com>
2018-06-25 22:34:19 -0400

    Signing the Transaction

--- src/transaction.ts
+++ src/transaction.ts
@@ -1,4 +1,8 @@
 import * as CryptoJS from "crypto-js";
+import * as elliptic from "elliptic";
+import {toHexString} from "./utils";
+
+const ec = new elliptic.ec("secp256k1");
 
 class TxOut { // transaction output
 
@@ -12,27 +16,39 @@ class TxOut { // transaction output
 }
 
 class TxIn { // transaction input
-    // uTxOutId
-    // uTxOutIndex
-    // Signature
+    public uTxOutId;
+    public uTxOutIndex;
+    public signature;
+
+    constructor(uTxOutId:string, uTxOutIndex:number, signature:string) {
+        this.uTxOutId = uTxOutId;
+        this.uTxOutIndex = uTxOutIndex;
+        this.signature = signature;
+    }
 }
 
 class Transaction {
-    // ID
-    // txIns[]
-    // txOuts[]
+    public id;
+    public txIns;
+    public txOuts;
+
+    constructor(id:number, txIns:TxIn[], txOuts:TxOut[]) {
+        this.id = id;
+        this.txIns = txIns;
+        this.txOuts = txOuts;
+    }
 }
 
 class UTxOut { // unspend transaction output
 
-    public uTxOutId;
-    public uTxOutIndex;
+    public txOutId;
+    public txOutIndex;
     public address;
     public amount;
 
-    constructor(uTxOutId: number, uTxOutIndex: number, address: string, amount: number) {
-        this.uTxOutId = uTxOutId;
-        this.uTxOutIndex = uTxOutIndex;
+    constructor(txOutId: number, txOutIndex: number, address: string, amount: number) {
+        this.txOutId = txOutId;
+        this.txOutIndex = txOutIndex;
         this.address = address;
         this.amount = amount;
     }
@@ -49,4 +65,32 @@ const getTxId = (tx): string => {
         .map(txOut => txOut.address + txOut.amount)
         .reduce((a, b) => a + b, "");
     return CryptoJS.SHA256(txInContent + txOutContent).toString();
+};
+
+const findUTxOut = (txOutId:string, txOutIndex:number, uTxOutList:UTxOut[]):UTxOut => {
+    return uTxOutList.find(
+        uTxOut => uTxOut.txOutId === txOutId && uTxOut.txOutIndex === txOutIndex
+    );
+};
+
+const signTxIn = (tx:Transaction, txInIndex:number, privateKey:string, uTxOut:UTxOut):string => {
+    const txIn = tx.txIns[txInIndex];
+    const dataToSign = tx.id;
+    const referencedUTxOut = findUTxOut(txIn.txOutId, txIn.txOutIndex, uTxOuts);
+    if (referencedUTxOut === null) {
+        return;
+    }
+    const key = ec.keyFromPrivate(privateKey, "hex");
+    const signature = toHexString(key.sign(dataToSign).toDER());
+    return signature;
+};
+
+const updateUTxOuts = (newTxs:Transaction, uTxOutList:UTxOut[]) => {
+    const newUTxOuts = newTxs
+        .map(tx => {
+            tx.txOuts.map((txOut, index) => {
+                new UTxOut(tx.id, index, txOut.address, txOut.amount);
+            });
+        })
+        .reduce((a, b) => a.contact(b), []);
 };
