63e9e4203f50c180fbd0e936845dc89174836b54
SUK HWA HAM <hamsukhwa@gmail.com>
2018-06-26 18:05:26 -0400

    remove error part to fix/edit utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 const toHexString = (byteArray):string => {
-    return Array.from(byteArray, byte => {
-        return ("0" + (byte & 0xff).toString(16)).slice(-2);
-    }).join("");
+    return byteArray
+        .reduce((output, elem) => 
+        output + ("0" + elem.toString(16)).slice(-2), "");
 };
+
 export { toHexString };
